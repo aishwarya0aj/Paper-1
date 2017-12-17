73ffffb0e1d3b42e258d35f0fe2f747c39585333
TechBroHJ <techbrohj@gmail.com>
2017-12-17 16:05:19 +0530

    New Changes

--- app/src/main/java/com/example/hrishikesh/paper/G_Scheme/Computer/CompSem3Subject_g_Activity.java
+++ app/src/main/java/com/example/hrishikesh/paper/G_Scheme/Computer/CompSem3Subject_g_Activity.java
@@ -1,13 +1,16 @@
 package com.example.hrishikesh.paper.G_Scheme.Computer;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.hrishikesh.paper.R;
 
 public class CompSem3Subject_g_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_comp_sem3_subject_g_);
     }
 }
